1abc89e8a10e697003ed86456e35bca56e2b4bde
Viviana <acero.vivianapulido@gmail.com>
2024-06-27 20:33:06 +0200

    alinear iconos navbar y agregar span

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
-import Titulo from './assets/Component/titulo/Titulo.jsx';
+import Navbar from './assets/Component/navbar/Navbar.jsx';
+import {TbWorld} from "react-icons/tb";
+import {TfiComment, TfiFlickrAlt, TfiStatsUp, TfiLayersAlt, TfiLayoutTab, TfiViewGrid, TfiViewList, TfiPieChart} from "react-icons/tfi"
+import {} from "react-icons/tfi"
 
+class Like extends React.Component {
+  render() {
+    return (
+    <div>
+      <TfiPieChart />
+      <TfiViewList />
+      <TfiViewGrid />
+      <TfiLayoutTab />
+      <TfiLayersAlt />
+      <TfiStatsUp />
+    <TbWorld/>
+    <TfiComment/>
+    <TfiFlickrAlt/>
+    
+    </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <Titulo/>
-  );
-}
+    <div>
+    <Navbar/>
+    </div>
+
+  )
+  
+};
 
 export default App;
